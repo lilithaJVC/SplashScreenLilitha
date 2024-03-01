027949aea8d8f6dafe9abbcf3c69af7ec04729b4
lab_services_student <lab_services_student@labVMH8OX>
2024-03-01 08:22:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/splashscreenlilitha/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.splashscreenlilitha
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+        }
+
+}
